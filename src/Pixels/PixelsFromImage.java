8f6d24c74ed91f3fb131c8b951ff0293522b3fc4
Cameron Motevasselani <cameron.motevasselani@armory.io>
2021-09-30 00:38:31 -0700

    more pixel tutorials

--- src/Pixels/PixelsFromImage.java
+++ src/Pixels/PixelsFromImage.java
@@ -2,8 +2,9 @@ package Pixels;
 
 import processing.core.PApplet;
 import processing.core.PImage;
+//import processing.core.PGraphics;
 
-import java.nio.file.Path;
+//import java.nio.file.Path;
 
 // Examples from https://processing.org/tutorials/pixels/#pixels-pixels-and-more-pixels
 public class PixelsFromImage extends PApplet {
@@ -20,19 +21,22 @@ public class PixelsFromImage extends PApplet {
   private PImage skull;
   private PImage destination;
 
-  public void settings(){
-    //height = 400;
-    //width = 400;
+  public void settings() {
+    height = 400;
+    width = 400;
     pixelSize = 16;
     colorMode = HSB;
     colorValue = getValue();
-    flower = loadImage("flower-image.jpg");
-    height = flower.height;
-    width = flower.width;
-    size(width, height);
+    //flower = loadImage("flower-image.jpg");
+    //flower = loadImage("sunflower.jpeg");
+    //height = flower.height;
+    //width = flower.width;
+    //size(width, height);
+    //size(flower.width, flower.height, P3D);
   }
 
   public void setup() {
+    flower = loadImage("/Users/cmotevasselani/git_repos/link108/processing-sketches/flower-image.jpg");
     //url = "https://ih1.redbubble.net/image.1502927078.3998/fposter,small,wall_texture,product,750x1000.jpg";
     //imageDownloader = new ImageDownloader(url);
     //Path localImage = imageDownloader.downloadImage();
@@ -44,7 +48,11 @@ public class PixelsFromImage extends PApplet {
 
     flower.pixels[0] = color(255,0,0); // Sets the first pixel of the image to red
     destination = createImage(flower.width, flower.height, RGB);
+    //image(flower, 0, 0);
+    smooth();
+    background(255);
   }
+
 
   public int getValue() {
     return colorMode == HSB ? 400 : 0;
@@ -60,6 +68,150 @@ public class PixelsFromImage extends PApplet {
     //pixelateImage();
     //pixelBrightnessThreshold();
     //simpleBrightnessThreshold();
+    //pixelLeftNeighborDiff();
+    //sharpenWithConvolution();
+    //pointilizeImage();
+    convert2Dto3D();
+  }
+
+
+  public int cellsize = 2;
+
+  public void convert2Dto3D() {
+    int cols = flower.width / cellsize;
+    int rows = flower.height / cellsize;
+    background(0);
+    loadPixels();
+    int x, y, loc, color;
+    float z;
+
+    for (int i = 0; i < cols; i++) {
+      for (int j = 0; j < rows; j++) {
+        x = i * cellsize + cellsize/2;
+        y = j * cellsize + cellsize/2;
+        loc = x + width * y;
+        color = flower.pixels[loc];
+
+        // Calculate a z position as a function of mouseX and pixel brightness
+        z = (float)((mouseX/(float)width) * brightness(flower.pixels[loc]) - 100.0);
+
+        // Translate to the location, set fill and stroke, and draw the rect
+        pushMatrix();
+        translate((float)x,(float)y,z);
+        fill(color);
+        noStroke();
+        rectMode(CENTER);
+        rect(0,0,cellsize,cellsize);
+        popMatrix();
+      }
+    }
+
+  }
+
+  public void pointilizeImage() {
+    int pointilize = 16;
+
+    // pick a random spot
+    int x = (int)(random(flower.width));
+    int y = (int)(random(flower.height));
+    int loc = x + flower.width * y;
+
+    flower.loadPixels();
+    float r = red(flower.pixels[loc]);
+    float g = green(flower.pixels[loc]);
+    float b = blue(flower.pixels[loc]);
+    noStroke();
+    System.out.println("loc: " + loc + ", X: " + x + ", Y: " + y + ", rgb: " + r + "," + g + "," + b);
+    //System.out.println("loc: " + loc + ", X: " + x + ", Y: " + y);
+    //System.out.println("rgb: " + r + "," + g + "," + b);
+    //System.out.println("color: " + color(r, g, b));
+
+    // draw an ellipse at that location
+    fill(r,g,b,100);
+    //fill(color(r, g, b), 100);
+    ellipse(x, y, pointilize, pointilize);
+  }
+
+  public void sharpenWithConvolution() {
+    int w = 80;
+    int loc;
+    frameRate(30);
+    float[][] matrix = { { -1, -1, -1 },
+                         { -1,  9, -1 },
+                         { -1, -1, -1 } };
+    int matrixsize = matrix.length;
+    image(flower, 0, 0);
+
+    // Where is the small rectangle we will process
+    int xstart = constrain(mouseX-w/2,0,flower.width);
+    int ystart = constrain(mouseY-w/2,0,flower.height);
+    int xend = constrain(mouseX+w/2,0,flower.width);
+    int yend = constrain(mouseY+w/2,0,flower.height);
+    loadPixels();
+
+    for (int x = xstart; x < xend; x++) {
+      for (int y = ystart; y < yend; y++) {
+        loc = x + flower.width * y;
+        // Each pixel location (x,y) gets passed into a function called convolution()
+        // which returns a new color value to be displayed.
+        int convolutedColor = convolution(x,y,matrix,matrixsize,flower);
+        pixels[loc] = convolutedColor;
+      }
+    }
+    updatePixels();
+  }
+
+  public int convolution(int x, int y, float[][] matrix, int matrixsize, PImage img) {
+    float rtotal = 0.0f;
+    float gtotal = 0.0f;
+    float btotal = 0.0f;
+    int loc, xLoc, yLoc;
+    int offset = matrixsize / 2;
+    for(int i=0; i<matrixsize; i++) {
+      for(int j=0; j<matrixsize; j++) {
+        xLoc = x + i - offset;
+        yLoc = y + j - offset;
+        loc = xLoc + img.width * yLoc;
+        // Make sure we have not walked off the edge of the pixel array
+        loc = constrain(loc,0,img.pixels.length-1);
+
+        // Calculate the convolution
+        // We sum all the neighboring pixels multiplied by the values in the convolution matrix.
+        rtotal += (red(img.pixels[loc]) * matrix[i][j]);
+        gtotal += (green(img.pixels[loc]) * matrix[i][j]);
+        btotal += (blue(img.pixels[loc]) * matrix[i][j]);
+      }
+    }
+
+    rtotal = constrain(rtotal, 0, 255);
+    gtotal = constrain(gtotal, 0, 255);
+    btotal = constrain(btotal, 0, 255);
+
+    return  color(rtotal, gtotal, btotal);
+
+  }
+
+  public void pixelLeftNeighborDiff() {
+    surface.setSize(flower.width, flower.height);
+    loadPixels();
+    flower.loadPixels();
+    int loc;
+    int leftloc;
+    int pixColor;
+    int leftPixColor;
+    for(int y = 1; y < flower.height; y++) {
+      for(int x = 0; x < flower.width; x++) {
+        loc = x + width * y;
+        pixColor = flower.pixels[loc];
+        leftloc = (x-1) + width * y;
+        leftPixColor = flower.pixels[leftloc];
+
+        float diff = abs(brightness(pixColor) - brightness(leftPixColor));
+        pixels[loc] = color(diff);
+        System.out.println("diff: " + diff);
+      }
+    }
+    updatePixels();
   }
 
   public void simpleBrightnessThreshold() {
