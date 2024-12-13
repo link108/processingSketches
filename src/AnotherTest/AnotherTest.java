135743fef058d5e0f7972897eb11a078ae0a80f8
Cameron Motevasselani <c7@Camerons-MacBook-Pro.local>
2024-12-12 19:24:37 -0800

    shift colors

--- src/AnotherTest/AnotherTest.java
+++ src/AnotherTest/AnotherTest.java
@@ -1,28 +1,66 @@
 package AnotherTest;
 
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.ArrayList;
 
 public class AnotherTest extends PApplet{
 
-  //public static void main(String[] args){
-  //  String[] processingArgs = {"FirstSketch.MySketch.FirstSketch.MySketch"};
-  //  FirstSketch.MySketch.FirstSketch.MySketch mySketch = new FirstSketch.MySketch.FirstSketch.MySketch();
-  //  PApplet.runSketch(processingArgs, mySketch);
-  //}
+  int[] colors;
+  ArrayList<PVector> ellipses;
+
+  int ellipseRadius = 30;
+  int colorIndex = 0;
+  int frameCounter = 0;
+  boolean updateColors = true;
+
+  public void setup(){
+    ellipses = new ArrayList<>();
+    colors = new int[] {
+            color(255, 0, 0),  // Red
+            color(0, 255, 0),  // Green
+            color(0, 0, 255),  // Blue
+            color(255, 255, 0), // Yellow
+            color(0, 255, 255)  // Cyan
+    };
+    background(64);
+  }
+
   public void settings(){
     size(500, 500);
   }
 
   public void draw(){
-    ellipse(mouseX, mouseY, 50, 50);
-  }
+    if(updateColors) {
+      // Colors do not shift in the below if condition, this makes them shift
+      if(ellipses.size() % colors.length == 0) {
+        colorIndex = (colorIndex + 1) % colors.length;
+      }
+      background(64);
+      for (PVector ellipsePos : ellipses) {
+        fill(colors[colorIndex]);
+        ellipse(ellipsePos.x, ellipsePos.y, ellipseRadius, ellipseRadius);
+        colorIndex = (colorIndex + 1) % colors.length;
+      }
+    }
+    updateColors = false;
 
-  public void mousePressed(){
-    background(64);
+    if(mousePressed) {
+      fill(colors[colorIndex]);
+      ellipse(mouseX, mouseY, ellipseRadius, ellipseRadius);
+      colorIndex = (colorIndex + 1) % colors.length;
+      ellipses.add(new PVector(mouseX, mouseY));
+    }
+
+    frameCounter++;
+    if (frameCounter % 30 == 0) {
+      updateColors = true;
+    }
   }
 
   public static void main(String[] args){
-    String[] processingArgs = {"FirstSketch.MySketch"};
+    String[] processingArgs = {"AnotherTest.AnotherTest"};
     AnotherTest anotherTest = new AnotherTest();
     PApplet.runSketch(processingArgs, anotherTest);
   }
